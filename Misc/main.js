d320fa3236b811c71c1f5de2e158eb58b9da62c2
itggot-andre-skvarc <andre.skvarc@elev.ga.ntig.se>
2019-04-11 17:57:08 +0200

    start

---
+++ Misc/main.js
@@ -0,0 +1,11 @@
+function toggla(event) {
+    let meny = document.querySelector(".meny")
+    meny.classList.toggle("show");
+}
+
+// function close(event) {
+//     let wrapper = document.querySelector(".wrapper")
+//     wrapper.classList.close("meny");
+
+
+// }
